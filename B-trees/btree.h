0d0e79223f868faef7d46fc83418d9d7f06134e1
Brecht Van de Vyvere <br5cht@hotmail.com>
2014-10-08 13:28:09 +0200

    zoekmethode verandert. Voegtoe en split in progress

--- B-trees/btree.h
+++ B-trees/btree.h
@@ -53,12 +53,15 @@ public:
         wortelindex=schijf.schrijf(wortel);
     }
 
-// geeft -1 als knoop niet gevonden is
-    blokindex zoek(Knoop &kn);
+    // geeft -1 als knoop niet gevonden is
+    blokindex zoek(const T& sleutel, const T& data);
 
-    void voegtoe(Knoop &kn);
+    void voegtoe(const T& sleutel, const T& data);
 
-    void verwijder(Knoop &kn);
+    void verwijder(const T& sleutel, const T& data);
+
+    void split(Knoop& kind, stack<blokindex> pad);
+
 private:
    Schijf<Knoop>& schijf;
    Knoop wortel;
@@ -66,43 +69,114 @@ private:
 };
 
 template<class T, class D, unsigned int m>
-blokindex Btree<T,D,m>::zoek(Knoop &kn){
+blokindex Btree<T,D,m>::zoek(const T& sleutel, const T& data){
     Knoop huidig_knoop = wortel;
     blokindex huidig_blokindex = wortelindex;
     bool gevonden = false;
 
-    schijf.lees(huidig_knoop,huidig_blokindex);
+    // wortel moet niet meer in geheugen gelezen worden
 
     while ( !gevonden ) {
         blokindex i = 0; // blokindexen overlopen
 
-            // k kinderen overlopen
-        // elke indewendige knoop met k kinderen bevat k-1 sleutels
-        while( i != huidig_knoop.k && kn.sleutel > huidig_knoop.sleutel[i] ) i++;
+        // sleutel zoeken in gerangschikte tabel met sleutels
+        while( i != huidig_knoop.k && sleutel > huidig_knoop.sleutel[i] ) i++;
 
-        // gestopt omdat hij kleinere of gelijke sleutel heeft
         if ( i != huidig_knoop.k ) {
             // gevonden
-            if( kn.sleutel == huidig_knoop.sleutel[i] ) return huidig_knoop.index[i];
+            if( sleutel == huidig_knoop.sleutel[i] ) return huidig_knoop.index[i];
 
             // kan niet meer verder zoeken
             else if ( huidig_knoop.isblad ){
                 return -1;
             }
             // kn.sleutel < huidig_knoop.sleutel[i]
-            // verderzoeken op blokindex i
+            // bij interne knoop verder zoeken in de juiste deelboom,
+            // waarvan de wortel een kind is van de huidige knoop
             else {
+                // wijzer naar kind heeft dzelfde index in de kindertabel als waar
+                // de niet gevonden sleutel zou moeten staan in de sleuteltabel
                 huidig_blokindex = huidig_knoop.index[i];
+                // inlezen in geheugen
                 schijf.lees(huidig_knoop, huidig_blokindex);
             }
         }
         // i == k
         else {
-            return 0;
+            return -1;
         }
     }
 }
 
+template<class T, class D, unsigned int m>
+void Btree<T,D,m>::voegtoe(const T& sleutel, const T& data){
+// toevoegen gebeurt steeds aan een blad
 
+// vanuit wortel zoeken we blad waarin de sleutel zou moeten zitten en voegen hem toe
+    Knoop huidig_knoop = wortel;
+    blokindex huidig_blokindex = wortelindex;
+
+    stack<blokindex> pad; // gebruikt voor ouders terug te vinden, indien gesplit moet worden
+    pad.push(huidig_blokindex);
+
+    // wortel moet niet meer in geheugen gelezen worden
+
+    while ( !huidig_knoop.isblad ) {
+        blokindex i = 0; // blokindexen overlopen
+
+        // sleutel zoeken in gerangschikte tabel met sleutels
+        while( i < huidig_knoop.k && sleutel > huidig_knoop.sleutel[i] ) i++;
+
+        if ( i >= huidig_knoop.k ) {
+            // wijzer naar kind heeft dzelfde index in de kindertabel als waar
+            // de niet gevonden sleutel zou moeten staan in de sleuteltabel
+            huidig_blokindex = huidig_knoop.index[i];
+            pad.push(huidig_blokindex);
+            // inlezen in geheugen
+            schijf.lees(huidig_knoop, huidig_blokindex);
+        }
+    }
+
+    // vol -> splitsen
+    if ( !huidig_knoop.k <= m-1 ) {
+        split(huidig_knoop, pad);
+    }
+
+    // genoeg plaats
+    // sleutels opschuiven
+    // er zijn k sleutels aanwezig -> k+1 indexen/kinderen
+    for(int v=huidig_knoop.k; v>i; v--) {
+        huidig_knoop.sleutel[v] = huidig_knoop.sleutel[v-1];
+        huidig_knoop.data[v] = huidig_knoop.data[v-1];
+        huidig_knoop.index[v+1] = huidig_knoop.index[v];
+    }
+
+    // knoop toevoegen
+    huidig_knoop.sleutel[i] = sleutel;
+    huidig_knoop.data[i] = data;
+}
+
+template<class T, class D, unsigned int m>
+void Btree<T,D,m>::split(Knoop &kind, stack<blokindex> pad) {
+// zolang overflow in ouder is, blijven splitten
+// of tot wortel moet splitten
+
+    bool overflow = true;
+
+    Knoop ouder;
+    schijf.lees(ouder, pad.pop());
+
+    while ( overflow && kind != wortel) {
+    // voeg midden toe bij ouder
+    // de twee nieuwe knopen toevoegen als kinderen van ouder
+
+
+    // todo
+
+        kind = ouder;
+        schijf.lees(ouder, pad.pop());
+        overflow = ( kind.k == m ? true : false );
+    }
+}
 
 #endif
