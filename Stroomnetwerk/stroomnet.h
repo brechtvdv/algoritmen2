f3dacbbcec06e9bacf11a6801bc26ecbc84de880
brechtvdv <br5cht@hotmail.com>
2015-08-06 12:00:26 +0200

    Add solution stroomnet.h

--- Stroomnetwerk/stroomnet.h
+++ Stroomnetwerk/stroomnet.h
@@ -1,19 +1,94 @@
 #ifndef __STROOMNET_H
 #define __STROOMNET_H
-
-#include "vergrotendpadzoeker.h"
 #include "graaf.h"
-using std::cout;
-using std::endl;
+#include <cassert>
+#include <iostream>
+using std::vector;
+using std::ostream;
 
-/**********************************************************************
+template< class T >
+class Pad:public std::vector< int >{
+public:
+    T geefCapaciteit() const{
+        return capaciteit;
+    }
+    void zetCapaciteit(const T& _capaciteit){
+        capaciteit=_capaciteit;
+    }
+friend ostream& operator<<(ostream& os, const Pad& p){
+    os<<"Capaciteit= "<<p.capaciteit<<" :: ";
+    if (p.size() > 0){
+        os<<p[0];
+    }
+    for (int i=1; i<p.size(); i++ ){
+        os<<"->"<<p[i];
+    }
+    os<<"\n";
+}
+protected:
+    T capaciteit;
+};
 
-   Class: Stroomnetwerk
 
-   beschrijving: Een stroomnetwerk gaat uit van een gewogen gerichte graaf
-                 die in de constructor wordt opgegeven
+template <class T>
+class Vergrotendpadzoeker{
+public:
+Vergrotendpadzoeker(const GraafMetTakdata<GERICHT,T>& _gr, int _van, int _naar, Pad<T>& _pad):
+                    pad(_pad), gr(_gr),van(_van),naar(_naar),
+                    voorganger(gr.aantalKnopen()), bezocht(gr.aantalKnopen(),false){
+    pad.clear();
+    assert(van != naar);
+    verwerk(van,0);
+    assert(pad.size()!=1);
+    if (pad.size() > 1){
+        T capaciteit=*gr.geefTakdata(pad[0],pad[1]);
+        for (int i=2; i<pad.size(); i++ ){
+            T nucapaciteit=*gr.geefTakdata(pad[i-1],pad[i]);
+            if (nucapaciteit<capaciteit)
+                capaciteit=nucapaciteit;
+            assert(capaciteit > 0);
+        }
+        pad.zetCapaciteit(capaciteit);
+    }
+}
+void verwerk(int knoopnr, int diepte){
+//    std::cerr <<" knoopnr "<<knoopnr  <<" <? "<< gr.aantalKnopen()<<"\n";
+    assert(knoopnr < gr.aantalKnopen());
+    bezocht[knoopnr]=true;
+    const typename GraafMetTakdata<GERICHT,T>::Knoop& kn=gr[knoopnr];
+    int nudiepte=diepte+1;
+    for (typename GraafMetTakdata<GERICHT,T>::Knoop::const_iterator it=kn.begin();
+                it!=kn.end();it++){
+        int kind=it->first;
+        if (*gr.geefTakdata(knoopnr,kind)> 0){
+            if (it->first==naar && nudiepte+1 > pad.size()){
+                voorganger[naar]=knoopnr;
+                pad.resize(nudiepte+1);
+                int nunr=naar;
+                int i=nudiepte;
+                while (nunr!=van){
+                    pad[i--]=nunr;
+                    nunr=voorganger[nunr];
+                }
+                assert(i==0);
+                assert(nunr==van);
+                pad[0]=nunr;
+            }
+            else if(!bezocht[kind]){
+                assert(*gr.geefTakdata(knoopnr,kind)> 0);
+                voorganger[kind]=knoopnr;
+                verwerk(kind,nudiepte);
+            }
+        }//if takdata> 0
+    }//for
+}
 
-***************************************************************************/
+const GraafMetTakdata<GERICHT,T>& gr;
+Pad<T>& pad;
+vector<int> voorganger;
+vector<bool> bezocht;
+int van,naar;
+};
 
 template <class T>//T = takdata
 class Stroomnetwerk:public GraafMetTakdata<GERICHT, T >{
@@ -75,7 +150,7 @@ Stroomnetwerk& operator+=(const Pad<T>& pad){
 void vergrootTak(int start, int eind, T delta){
         int taknr=this->verbindingsnummer(start,eind);
         if (taknr==-1)
-            taknr=this->voegVerbindingToe(start,eind,delta);
+            taknr=voegVerbindingToe(start,eind,delta);
         else
             this->takdatavector[taknr]+=delta;
 }
