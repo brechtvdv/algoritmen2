e8a61d4cfd8f840e8c9651076c4f62885a47a768
brechtvdv <br5cht@hotmail.com>
2015-08-06 12:45:37 +0200

    Init KMP

---
+++ Knut-Morris-Pratt/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: brechtvdv
+ *
+ * Created on 06 August 2015, 12:40
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
