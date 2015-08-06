fa6bc535693a433f4e1872ae5aa4b68207c5107c
brechtvdv <br5cht@hotmail.com>
2015-08-06 16:37:34 +0200

    Setup read bible.txt and input

--- Knut-Morris-Pratt/knuthmorrispratt.h
+++ Knut-Morris-Pratt/knuthmorrispratt.h
@@ -2,9 +2,23 @@
 typedef unsigned int uint;
 typedef unsigned char uchar;
 class KnuthMorrisPratt{
+    const uchar* naald_;
+    uint naaldLengte_;
+    
 public:
     KnuthMorrisPratt(const uchar* naald,uint _naaldlengte);
     void zoek(std::queue<const uchar*>& plaats,
                      const uchar* hooiberg, uint hooiberglengte);
 };
+
+KnuthMorrisPratt::KnuthMorrisPratt(const uchar* naald, uint _naaldlengte)
+{
+    naald_ = naald;
+    naaldLengte_ = _naaldlengte;
+}
+
+void KnuthMorrisPratt::zoek(std::queue<const uchar*>& plaats, const uchar* hooiberg, uint hooiberglengte)
+{
+    
+}
     
--- Knut-Morris-Pratt/main.cpp
+++ Knut-Morris-Pratt/main.cpp
@@ -6,6 +6,14 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <queue>
+#include "knuthmorrispratt.h"
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
 
 using namespace std;
 
@@ -13,7 +21,27 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+    
+    cout << "Give me a string to search in the bible:" << endl;
+        
+    string input;
+    cin >> input;
 
+    uchar* naald = (uchar *) input.c_str();
+    uint naaldLengte = input.length();
+    
+    // Hooiberg is de bijbel
+    ifstream in("bijbel.txt");
+    string contents((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+    
+    uchar* hooiberg = (uchar *) contents.c_str();
+    uint hooibergLengte = contents.length();
+
+    KnuthMorrisPratt kmp(naald, naaldLengte);
+    queue<const uchar*> plaats;
+    kmp.zoek(plaats, hooiberg, hooibergLengte);
+    
+    cout << naald << " komt "  << plaats.size() << " keer voor." << endl;
+    
     return 0;
 }
-
